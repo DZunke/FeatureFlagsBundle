246782b98583227221c21df4e47528e9c05e733a
Bennie Zwerver <benniezwerver@hotmail.com>
2016-02-23 19:31:07 +0100

    Reorganized methods in this file and made some safeguards to prevent the application from breaking

--- DependencyInjection/Compiler/Conditions.php
+++ DependencyInjection/Compiler/Conditions.php
@@ -10,18 +10,12 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class Conditions implements CompilerPassInterface
 {
-
     /**
      * @param ContainerBuilder $container
      */
     public function process(ContainerBuilder $container)
     {
         $configs = $container->getExtensionConfig('d_zunke_feature_flags');
-
-        if (1 <= count($configs)) {
-            return;
-        }
-
         $config = reset($configs);
 
         $this->configureToggle($config, $container);
@@ -33,31 +27,23 @@ class Conditions implements CompilerPassInterface
      */
     private function configureToggle(array $config, ContainerBuilder $container)
     {
+        $config = $this->normalizeConfig($config);
+
         $definition = $container->getDefinition('dz.feature_flags.toggle');
         $definition->addMethodCall('setDefaultState', [$config['default']]);
 
         $this->fillConditionsBag($container);
 
         foreach ($config['flags'] as $name => $flagConfig) {
+            $flagConfig = $this->normalizeFlagConfig($flagConfig, $config['default']);
 
-            $flagDefinition = new Definition(
-                Flag::class, //'DZunke\FeatureFlagsBundle\Toggle\Flag',
-                [
-                    $name,
-                    new Reference('dz.feature_flags.conditions_bag'),
-                    $flagConfig['default']
-                ]
+            $flagDefinition = $this->createFlagDefinition(
+                $name,
+                new Reference('dz.feature_flags.conditions_bag'),
+                $flagConfig['default']
             );
 
-            foreach ($flagConfig['conditions_config'] as $condition => $config) {
-                $flagDefinition->addMethodCall(
-                    'addCondition',
-                    [
-                        $condition,
-                        $config
-                    ]
-                );
-            }
+            $this->processFlagConditions($flagDefinition, $flagConfig['conditions_config']);
 
             $container->setDefinition('dz.feature_flags.toggle.flag.' . $name, $flagDefinition);
             $definition->addMethodCall('addFlag', [$flagDefinition]);
@@ -81,6 +67,91 @@ class Conditions implements CompilerPassInterface
             $options = reset($options);
             $containerBag->addMethodCall('set', [$options['alias'], new Reference($service)]);
 
+        }
+    }
+
+    /**
+     * Normalize config object so it can always be processed even
+     * if there are values missing from the config object
+     *
+     * @param array|null $config
+     *
+     * @return array
+     */
+    private function normalizeConfig(array $config = null)
+    {
+        if (false === is_array($config)) {
+            $config = [];
+        }
+
+        if (true === array_key_exists('flags', $config) && false === is_array($config['flags'])) {
+            unset($config['flags']);
+        }
+
+        return array_merge([
+            'default' => true,
+            'flags' => [],
+        ], $config);
+    }
+
+    /**
+     * Normalizes flag config to prevent the code from breaking
+     * whenever a property or maybe multiple properties do not exist
+     *
+     * @param array|null $config
+     * @param bool $defaultActive
+     *
+     * @return array
+     */
+    private function normalizeFlagConfig(array $config = null, $defaultActive)
+    {
+        if (false === is_array($config)) {
+            $config = [];
+        }
+
+        if (true === array_key_exists('conditions_config', $config) && false === is_array($config['conditions_config'])) {
+            unset($config['conditions_config']);
+        }
+
+        return array_merge([
+            'default' => (bool) $defaultActive,
+            'conditions_config' => [],
+        ], $config);
+    }
+
+    /**
+     * @param string $name
+     * @param Reference $reference
+     * @param bool $default
+     *
+     * @return Definition
+     */
+    private function createFlagDefinition($name, Reference $reference, $default)
+    {
+        return new Definition(
+            Flag::class,
+            [
+                (string) $name,
+                $reference,
+                (bool) $default
+            ]
+        );
+    }
+
+    /**
+     * @param Definition $flag
+     * @param array $config
+     */
+    private function processFlagConditions(Definition $flag, array $config)
+    {
+        foreach($config as $condition => $optionalValues) {
+            $flag->addMethodCall(
+                'addCondition',
+                [
+                    $condition,
+                    $optionalValues
+                ]
+            );
         }
     }
 }
