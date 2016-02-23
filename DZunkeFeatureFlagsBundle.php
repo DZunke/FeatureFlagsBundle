b04f0d1a15afa61caa021e944e1ca76944191ded
Bennie Zwerver <benniezwerver@hotmail.com>
2016-02-23 19:30:31 +0100

    Add the compiler pass to build process

--- DZunkeFeatureFlagsBundle.php
+++ DZunkeFeatureFlagsBundle.php
@@ -7,4 +7,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class DZunkeFeatureFlagsBundle extends Bundle
 {
 
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new Conditions());
+    }
+
 }
